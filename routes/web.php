c1e6a895f6227e61ce71e412931e0fb4e8bcf897
aliyah <aliyah24si@mahasiswa.pcr.ac.id>
2025-10-08 11:02:13 +0700

    pertemuan 5

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-Route::get('/', function () {
+// HEAD
+Route::get('ketua/', function () {
     return view('ketua');
-=======
+});
 Route::get('anggota/', function () {
     return view('anggota');
->>>>>>> github/nisa
+// github/nisa
 });
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
