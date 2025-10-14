576cc6e6ce75a662c8d57620d9c9668531ef41e6
yuannisa24si-beep <yuannisa24si@mahasiswa.pcr.ac.id>
2025-10-14 16:45:23 +0700

    yuannisapertemuan5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ketua', function () {
+    return view('ketua');
+});
+Route::get('/anggota', function () {
+    return view('anggota');
+});
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
