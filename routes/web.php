e7e0f6a1a4d0bf1beed9332eb617a9562967f6ed
aliyah <aliyah24si@mahasiswa.pcr.ac.id>
2025-10-21 20:38:32 +0700

    lembaga_desa

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\LembagadesaController;
 
-=======
->>>>>>> 576cc6e6ce75a662c8d57620d9c9668531ef41e6
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
+
 Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
 Route::resource('lembagadesa', LembagadesaController::class);
 
 
-=======
+
 Route::get('/ketua', function () {
     return view('ketua');
 });
@@ -28,4 +27,3 @@ Route::get('/anggota', function () {
 
 Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
->>>>>>> 576cc6e6ce75a662c8d57620d9c9668531ef41e6
